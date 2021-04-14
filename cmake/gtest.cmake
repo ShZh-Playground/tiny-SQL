0d7ab08fbe90b630afa3d65d89b3031aa2bf8004
Zhihao Shen <zhihaoshen7@qq.com>
2021-04-14 09:25:33 +0800

    🗑 chore: rewrite CMake files with advanced syntax.

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,12 +1,12 @@
-# 在config的阶段（生成build system）的时候引入
-# 另外一个ExternalProject是在编译的时候引入
+# Introduce google test
 include(FetchContent)
 FetchContent_Declare(gtest
   GIT_REPOSITORY  https://github.com/google/googletest.git
   GIT_TAG         release-1.10.0
   SOURCE_DIR      ${PROJECT_SOURCE_DIR}/3rd/gtest
 )
-# configure build of googletest
+# New Visual Studio projects link the C runtimes dynamically 
+# But GoogleTest links them statically
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(gtest)
