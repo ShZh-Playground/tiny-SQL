b9e791982222e232a1c18946adb9629835fb6cc0
Zhihao Shen <zhihaoshen7@qq.com>
2021-04-14 15:55:28 +0800

    ✨refactor: refactor py and cmake files with editorconfig.

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,9 +1,9 @@
 # Introduce google test
 include(FetchContent)
 FetchContent_Declare(gtest
-  GIT_REPOSITORY  https://github.com/google/googletest.git
-  GIT_TAG         release-1.10.0
-  SOURCE_DIR      ${PROJECT_SOURCE_DIR}/3rd/gtest
+    GIT_REPOSITORY  https://github.com/google/googletest.git
+    GIT_TAG         release-1.10.0
+    SOURCE_DIR      ${PROJECT_SOURCE_DIR}/3rd/gtest
 )
 # New Visual Studio projects link the C runtimes dynamically 
 # But GoogleTest links them statically
--- script/install.py
+++ script/install.py
@@ -6,20 +6,20 @@ import shutil
 cmake_build_type = "Release"
 
 if __name__ == "__main__":
-  # Configure
-  os.system("cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=" + cmake_build_type)
-  # Build
-  os.system("cmake --build build --config " + cmake_build_type)
+    # Configure
+    os.system("cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=" + cmake_build_type)
+    # Build
+    os.system("cmake --build build --config " + cmake_build_type)
 
-  if (os.path.exists("bin")):
-    # Windows target path: .\bin\Debug\xxx.exe
-    # Move them to .\bin\xxx.exe
-    if (os.name == "nt"):
-      for root, _, files in os.walk("bin"):
-        for f in files:
-          src = os.path.join(root, f)
-          dst = os.path.join("bin", f)
-          shutil.move(src, dst)
-          os.rmdir(os.path.join("bin", cmake_build_type))
-  else:
-    print("Error building!")
+    if (os.path.exists("bin")):
+        # Windows target path: .\bin\Debug\xxx.exe
+        # Move them to .\bin\xxx.exe
+        if (os.name == "nt"):
+            for root, _, files in os.walk("bin"):
+                for f in files:
+                    src = os.path.join(root, f)
+                    dst = os.path.join("bin", f)
+                    shutil.move(src, dst)
+                    os.rmdir(os.path.join("bin", cmake_build_type))
+    else:
+        print("Error building!")
