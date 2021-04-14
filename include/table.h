89be6d543e2ccedd977f29ad81a62accb6abfdfd
Zhihao Shen <zhihaoshen7@qq.com>
2021-04-14 11:56:32 +0800

    ✨refactor: remove std namespace for C types and functions.

--- include/table.h
+++ include/table.h
@@ -13,11 +13,11 @@ constexpr int kNameMaxLength = 20;
 constexpr int kEmailMaxLength = 50;
 
 struct Row {
-  std::uint32_t id;
+  ::uint32_t id;
   char name[kNameMaxLength];
   char email[kEmailMaxLength];
 
-  constexpr static std::uint32_t getSize() {
+  constexpr static ::uint32_t getSize() {
     return sizeof(id) + sizeof(name) + sizeof(email);
   }
 };
@@ -26,17 +26,17 @@ void saveToMem(const Row& row, Byte* addr);
 
 Row loadFromMem(Byte* addr);
 
-constexpr std::uint32_t kPageSize =
+constexpr ::uint32_t kPageSize =
     4096;  // 和大多数操作系统分页的大小一样，4096B
-constexpr std::uint32_t kMaxPageNum = 50;
+constexpr ::uint32_t kMaxPageNum = 50;
 // Table由各种页面组成
 class Table {
  private:
-  std::uint32_t index_;  // Table需要自己维护一个递增的索引
+  ::uint32_t index_;  // Table需要自己维护一个递增的索引
   Byte* pages_[kMaxPageNum];
   // 内存管理——如何把一个对象放在内存中
   // 答案是获取一个页面最多能放几个对象
-  const std::uint32_t rowCountPerPage = kPageSize / Row::getSize();
+  const ::uint32_t rowCountPerPage = kPageSize / Row::getSize();
 
   Byte* getInsertAddr();
 
@@ -57,8 +57,8 @@ class Table {
 
   friend std::ostream& operator<<(std::ostream& os, const Table& table) {
     for (int i = 0; i < table.index_; ++i) {
-      std::uint32_t pageIndex = i / table.rowCountPerPage;
-      std::uint32_t rowOffset = i % table.rowCountPerPage;
+      ::uint32_t pageIndex = i / table.rowCountPerPage;
+      ::uint32_t rowOffset = i % table.rowCountPerPage;
       Byte* addr = table.pages_[pageIndex] + rowOffset * Row::getSize();
 
       auto row = loadFromMem(addr);
